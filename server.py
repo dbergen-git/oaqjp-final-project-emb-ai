a438b7bbcac8ce91bdac015752c4de5f1d81044f
dbergen-git <dbergen@dbergen.com>
2026-05-20 15:38:03 -0400

    static code analysis completed

--- server.py
+++ server.py
@@ -1,4 +1,13 @@
 
+"""
+server.py
+
+Flask web application for emotion detection using Watson AI.
+Routes:
+- /emotionDetector: Analyze emotional content of text.
+- /: Render index page.
+"""
+
 # import Flask class
 from flask import Flask, request, render_template
 
@@ -12,44 +21,47 @@ app = Flask(__name__)
 # define the route for the emotionDetector
 @app.route("/emotionDetector")
 def analyze_emotion_route():
-    textToAnalyze = str(request.args.get("textToAnalyze"))
+    """
+    Function that analyzes emotional content of a text string
+    Use Watson AI to make the determination
+
+    """
+
+    text_to_analyze = str(request.args.get("textToAnalyze"))
 
     # analyze text - return dict of emotion(s) + score
-    emotion_dict = emotion_detector(textToAnalyze)
+    emotion_dict = emotion_detector(text_to_analyze)
 
     # format str opertions - into the needed string
     dominant_emotion = emotion_dict["dominant_emotion"]
 
     # error handling: test to determine if dominant_emotion is None
     # if so - then inform user of incorrect text + try again
-    if dominant_emotion is None:        
-         formatted_result = "Invalid text! Please try again!"
-
-         return formatted_result
-
-    # error handling: case where dominant_emotion is NOT None
-    else:        
-        # prepare the emotion values - exclude the dominant emotion
-        # using list comprehension - generate the formatted list - as '<emotion>': value
-        emotions = [f"'{k}': {v}" for k, v in emotion_dict.items() if k != "dominant_emotion"]
-
-        # join the emotions with commas and the last two emotions separated by 'and'
-        emotions_str = ', '.join(emotions[:-1]) + f" and {emotions[-1]}"
-
-        # format the final string
-        formatted_result = (f"For the given statement, the system response is {emotions_str}. "
-                            f"The dominant emotion is {dominant_emotion}.")                        
+    if dominant_emotion is None:
+        formatted_result = "Invalid text! Please try again!"
 
         return formatted_result
+
+    # error handling: case where dominant_emotion is NOT None
+    # prepare the emotion values - exclude the dominant emotion
+    # using list comprehension - generate the formatted list - as '<emotion>': value
+    emotions = [f"'{k}': {v}" for k, v in emotion_dict.items() if k != "dominant_emotion"]
+
+    # join the emotions with commas and the last two emotions separated by 'and'
+    emotions_str = ', '.join(emotions[:-1]) + f" and {emotions[-1]}"
+
+    # format the final string
+    formatted_result = (f"For the given statement, the system response is {emotions_str}. "
+                        f"The dominant emotion is {dominant_emotion}.")                        
+
+    return formatted_result
 
 
 @app.route("/")
 def render_index_page():
+    """Render the main index.html page for the web app."""
     return render_template("index.html")
 
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=5001)
-
-
-
+    app.run(host="localhost", port=5000)
